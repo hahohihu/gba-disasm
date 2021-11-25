ed07a43cdcec887710a9cec39a4d5b6537fc93ab
hahohihu <pshen4@gmail.com>
2021-11-25 12:51:21 -0500

    decode skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -98,3 +98,26 @@ struct HiRegisterOp {
     src: LoHiRegister,
     dst: LoHiRegister
 }
+
+// TODO
+struct CPU {}
+
+trait ThumbInstruction {
+    fn execute(&self, cpu: &mut CPU);
+}
+
+fn decode_thumb(raw: u16) -> Box<dyn ThumbInstruction> {
+    match raw >> 13 {
+        0b000 => {},
+        0b001 => {},
+        0b010 => {},
+        0b011 => {},
+        0b100 => {},
+        0b101 => {},
+        0b110 => {},
+        0b111 => {},
+        _ => unreachable!("DEV ERROR")
+    }
+    unimplemented!();
+}
+
